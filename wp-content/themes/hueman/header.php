7093169b327d541048ffcba53b3a720288c6aecf
nonab <pawel.banas@gmail.com>
2015-02-21 16:37:01 +0100

    nofollow
    disable robots

--- wp-content/themes/hueman/header.php
+++ wp-content/themes/hueman/header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <meta name='robots' content='noindex,nofollow' />
 	
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 	
